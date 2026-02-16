95c47f19ef197c095348f46de808ca34291ea7a7
bensam95 <bensam.dev@gmail.com>
2026-02-16 16:02:13 +0100

    Mise à jour script sql
    Replaced the EMPLOYE and LIGUE tables with updated structures. The EMPLOYE table now includes additional fields and foreign key constraints.

--- ScriptCreationTable.sql
+++ ScriptCreationTable.sql
@@ -1,32 +1,24 @@
 DROP TABLE EMPLOYE IF EXISTS;
 DROP TABLE LIGUE IF EXISTS;
 
-CREATE TABLE EMPLOYE
-(
-    NumEmployé INT AUTO_INCREMENT NOT NULL,
-    PrenomEmployé VARCHAR(25),
-    NomEmployé VARCHAR(25),
-    MDPEmployé VARCHAR(25),
-    Rôle VARCHAR(25),
-    EmailEmployé VARCHAR(25),
-    DateArrivée DATE,
-    DateDepart DATE,
-    Login VARCHAR(25),
-    CONSTRAINT PK_EMPLOYE PRIMARY KEY (NumEmployé),
-    CONSTRAINT UK_LIGUE foreign KEY(NumLigue) REFERENCES(NumEmployé)
-)
-    engine=innodb;
+CREATE TABLE LIGUE(
+   id_ligue INT AUTO_INCREMENT,
+   nom_ligue VARCHAR(255) NOT NULL,
+   PRIMARY KEY(id_ligue),
+   UNIQUE(nom_ligue)
+);
 
-desc EMPLOYE;
-
-CREATE TABLE LIGUE
-(
-    NumLigue INT AUTO_INCREMENT NOT NULL,
-    NomLigue VARCHAR(25),
-    NumEmployé INT,
-    CONSTRAINT PK_LIGUE PRIMARY KEY (NumLigue),
-    CONSTRAINT UK_LIGUE_NOM UNIQUE(NomLigue)
-)
-    engine=innodb;
-
-desc LIGUE;
+CREATE TABLE EMPLOYE(
+   id_employe INT AUTO_INCREMENT,
+   prenom_employe VARCHAR(255) NOT NULL,
+   mail_employe VARCHAR(255) NOT NULL,
+   password_employe VARCHAR(255) NOT NULL,
+   nom_employe VARCHAR(255) NOT NULL,
+   rôle_employe VARCHAR(50),
+   date_arrivee_employe DATE,
+   date_départ_employe VARCHAR(50),
+   id_ligue INT,
+   PRIMARY KEY(id_employe),
+   UNIQUE(mail_employe),
+   FOREIGN KEY(id_ligue) REFERENCES LIGUE(id_ligue)
+);

33119ee5da9215d4734b0594e5d07882a08becd3
hirschbouessot <hirschbouessot@gmail.com>
2026-03-26 20:15:28 +0100

    ModifScriptCreationTable
    Rajout de l'option de gestion de clé étrangère
    Correction des instructions  "DROP ..."
    Modification de la variable de "date_depart_employe"

--- ScriptCreationTable.sql
+++ ScriptCreationTable.sql
@@ -1,5 +1,5 @@
-DROP TABLE EMPLOYE IF EXISTS;
-DROP TABLE LIGUE IF EXISTS;
+DROP TABLE IF EXISTS EMPLOYE;
+DROP TABLE IF EXISTS LIGUE  ;
 
 CREATE TABLE LIGUE(
    id_ligue INT AUTO_INCREMENT,
@@ -10,15 +10,16 @@ CREATE TABLE LIGUE(
 
 CREATE TABLE EMPLOYE(
    id_employe INT AUTO_INCREMENT,
+   nom_employe VARCHAR(255) NOT NULL,
    prenom_employe VARCHAR(255) NOT NULL,
    mail_employe VARCHAR(255) NOT NULL,
    password_employe VARCHAR(255) NOT NULL,
-   nom_employe VARCHAR(255) NOT NULL,
    rôle_employe VARCHAR(50),
    date_arrivee_employe DATE,
-   date_départ_employe VARCHAR(50),
+   date_depart_employe DATE,
    id_ligue INT,
    PRIMARY KEY(id_employe),
    UNIQUE(mail_employe),
-   FOREIGN KEY(id_ligue) REFERENCES LIGUE(id_ligue)
+   FOREIGN KEY(id_ligue) REFERENCES LIGUE(id_ligue) 
+   ON DELETE SET NULL
 );
